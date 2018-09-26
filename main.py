608c5bd5c2d9091cd485cff34feaea0f0c9a83fc
root <root@ss.tpvg5lpsdaqu3cwjn5jjvuktfg.syx.internal.cloudapp.net>
2018-09-26 06:45:01 +0000

    this is the py server for wechat chatbot

---
+++ main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env 
+#coding=utf-8
+import jenkins
+from time import sleep
+import itchatmp
+
+jenkins_server_url='http://ssncg.koreacentral.cloudapp.azure.com:8080'
+#在用户设定中找到uid 和 token
+user_id='scott'
+api_token='3802d199cd9bb4e3b8f12c7124b6dfb5'
+server = jenkins.Jenkins(jenkins_server_url, username=user_id, password=api_token)
+jobOn = 'testOn'
+jobOff = 'testOff'
+
+def run_jenkins_job(jobname):
+    next_build_number = server.get_job_info(jobname)['nextBuildNumber']
+    server.build_job(jobname)
+    sleep(2)
+# 返回执行结果
+    return server.get_build_info(jobname, next_build_number)[u'result']
+
+itchatmp.update_config(itchatmp.WechatConfig(
+    token='scott',
+    appId = 'wx297e7d13e096de5a',
+    appSecret = 'b3d11f31887ab4780c0355fb4f098e4b'))
+
+@itchatmp.msg_register(itchatmp.content.TEXT)
+def text_reply(msg):
+    if msg['Content'] == 'D:ON':
+        result=run_jenkins_job(jobOn)
+        return result
+    elif msg['Content'] == 'D:OFF':
+        result=run_jenkins_job(jobOff)
+        return result
+
+itchatmp.run(port=80)
